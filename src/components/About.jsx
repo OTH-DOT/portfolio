c6d140cc5ed5741273a08e69fffb8564e2de6386
OTH-DOT <123239375+OTH-DOT@users.noreply.github.com>
2024-06-09 13:16:49 +0100

    h1_m23

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,64 @@
 import React from 'react'
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
 
-const About = () => {
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
+
+import { SectionWrapper } from '../hoc'
+
+const ServiceCard = ({ index, title, icon }) => {
   return (
-    <div>About</div>
+    <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450
+      }}
+      className="xs:w-[250px] w-full"
+    >
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card "
+      >
+        <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Hi, I'm a full stack developer with expertise in both 
+        front-end and back-end technologies. 
+        I create dynamic, user-friendly web applications using HTML, CSS, JavaScript, 
+        and frameworks like React or Angular. 
+        On the server side, I work with languages like Python, Node.js, 
+        and databases such as MySQL and MongoDB. 
+        I'm also experienced with Git and cloud services like AWS. My holistic approach 
+        ensures robust, scalable, and efficient applications. 
+        I'm passionate about tackling new challenges and contributing to innovative projects!
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10 ">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about") 
